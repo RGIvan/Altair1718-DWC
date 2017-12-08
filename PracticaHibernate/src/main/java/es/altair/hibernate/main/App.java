4b7815ffa77f7503dbec615fc0db9bd3867f88d3
Crono <Crono@Crono>
2017-12-08 22:35:12 +0100

    DWC

--- PracticaHibernate/src/main/java/es/altair/hibernate/main/App.java
+++ PracticaHibernate/src/main/java/es/altair/hibernate/main/App.java
@@ -1,41 +1,83 @@
 package es.altair.hibernate.main;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import es.altair.hibernate.bean.Cliente;
 import es.altair.hibernate.bean.Tienda;
 import es.altair.hibernate.dao.ClienteDAO;
 import es.altair.hibernate.dao.ClienteDAOImplHibernate;
 import es.altair.hibernate.dao.TiendaDAO;
 import es.altair.hibernate.dao.TiendaDAOImplHibernate;
 
-public class App 
-{
-    public static void main( String[] args ){
-    	
-    	ClienteDAO cDAO = new ClienteDAOImplHibernate();
-    	TiendaDAO tDAO = new TiendaDAOImplHibernate();
-    	
-    	Tienda t1 = new Tienda("tienda S.L", "C/ tienda1");
-    	
-    	Cliente c1 = new Cliente("Pepe", "García", 123456789, "pepe@gmail.com", t1);
-    	Cliente c2 = new Cliente("Paco", "García", 123456789, "paco@gmail.com", t1);
-    	Cliente c3 = new Cliente("Marta", "García", 123456789, "marta@gmail.com", t1);
-    	
-    	Set<Cliente> clientes = new HashSet<Cliente>();
-    	
-    	clientes.add(c1);
-    	clientes.add(c2);
-    	clientes.add(c3);
-    	t1.setClientes(clientes);
-    	
-    	System.out.println("----- GUARDAR TIENDA ------");
-    	
-    	tDAO.guardar(t1);
-    	
-    	System.out.println("----- GUARDAR CLIENTE ------");
-    	
-    	cDAO.guardar(c1);
-    }
+public class App {
+
+	public static ClienteDAO cDAO = new ClienteDAOImplHibernate();
+	public static TiendaDAO tDAO = new TiendaDAOImplHibernate();
+
+	public static void main(String[] args) {
+
+		boolean salir = true;
+
+		do {
+
+			try {
+
+				System.out.println("╔══════════════════════════════════════════════════════════════╗");
+				System.out.println("║        BIENVENID@ AL PROGRAMA DE GESTIÓN DE TIENDAS          ║");
+				System.out.println("╠══════════════════════════════════════════════════════════════╣");
+				System.out.println("║                                                              ║");
+				System.out.println("║  1.- Crear una tienda.                                       ║");
+				System.out.println("║                                                              ║");
+				System.out.println("║  2.- Ingresar en una tienda.                                 ║");
+				System.out.println("║                                                              ║");
+				System.out.println("║  3.- Listar tiendas.                                         ║");
+				System.out.println("║                                                              ║");
+				System.out.println("║  0) Salir.                                                   ║");
+				System.out.println("║                                                              ║");
+				System.out.println("╚══════════════════════════════════════════════════════════════╝");
+
+				System.out.println("");
+				System.out.print("Seleccione una de las opciones: ");
+				String var = Leer.dato();
+
+				int opcion = Integer.parseInt(var);
+
+				switch (opcion) {
+
+				case 0:
+					salir = false;
+					System.out.println("");
+					System.out.println("Gracias por usar nuestro programa.");
+					System.out.println("");
+					break;
+
+				case 1:
+
+					System.out.println("");
+					System.out.println("Introduzca el nombre de la tienda.");
+					System.out.println("");
+
+					String tienda = Leer.dato();
+
+					System.out.println("");
+					System.out.println("Introduzca la dirección de la tienda.");
+					System.out.println("");
+
+					String direccion = Leer.dato();
+
+					Tienda t = new Tienda(tienda, direccion);
+
+					tDAO.guardar(t);
+
+				case 2:
+					break;
+
+				case 3:
+
+				}
+
+			} catch (NumberFormatException e) {
+				System.out.println("");
+				System.err.println("Introduce un número.");
+			}
+
+		} while (salir == true);
+	}
 }
