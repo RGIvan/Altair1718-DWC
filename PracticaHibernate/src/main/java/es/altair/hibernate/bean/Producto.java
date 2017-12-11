0a3add91b80feff4f844544253242ae55bbaf741
Crono <Crono@Crono>
2017-12-11 23:57:45 +0100

    DWC

--- PracticaHibernate/src/main/java/es/altair/hibernate/bean/Producto.java
+++ PracticaHibernate/src/main/java/es/altair/hibernate/bean/Producto.java
@@ -76,6 +76,14 @@ public class Producto implements Serializable {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 
 	@Override
 	public String toString() {
