4cb6a4034d6e753ff94b4432624b9153fe347be0
RGIvan <cloudchito@gmail.com>
2018-01-11 10:50:20 +0100

    DWC

---
+++ WebApp/src/main/java/es/altair/dao/LibroDAO.java
@@ -0,0 +1,22 @@
+package es.altair.dao;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import es.altair.bean.Libro;
+import es.altair.bean.Usuario;
+
+public interface LibroDAO {
+
+	List<Libro> listar (Usuario u);
+	
+	byte[] obtenerPortadaPorId(int idLibro);
+
+	void borrar(String uuid);
+	
+	Libro obtenerLibroPorUUID(String uuid);
+
+	void actualizar(String titulo, String autor, int isbn, ByteArrayOutputStream os, String uuid, Usuario usuario);
+
+	void insertar(Libro l);
+}
